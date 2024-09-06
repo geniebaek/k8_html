9fba1f5a208b4a03af462b97296ac76f7ef65565
geniebaek <baichen99@naver.com>
2024-09-06 14:21:44 +0900

    JavaScript DDM 제어

--- 04/04.js
+++ 04/04.js
@@ -18,6 +18,5 @@ document.addEventListener('DOMContentLoaded',()=>{
     let n = Math.floor(Math.random()*6)+1;
     console.log('n=',n);
     msg.innerHTML=`<h2>${bt3.textContent}:<span>${n}</span></h2>`;
-    
    });
 });
