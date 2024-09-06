de80f15ed03b55eb2a41be6d12146776708b0c0d
geniebaek <baichen99@naver.com>
2024-09-06 14:22:57 +0900

    JavaScript 주사위

--- 05/05.js
+++ 05/05.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded',()=>{
 const bt=document.querySelector('button');
 const img=document.querySelector('#msg > img');
 
+
 bt.addEventListener('click',()=>{
   let n = Math.floor(Math.random()*6)+1; //1-6
 
