3311b3de393e959e4fa007978c5967fff6e728fe
geniebaek <baichen99@naver.com>
2024-09-11 12:46:06 +0900

    JavaScript 배열 - 박스오피스

--- 12/12_select.js
+++ 12/12_select.js
@@ -24,10 +24,10 @@ const getData = (selDt, ul, gubun) => {
             <sapn class='movieNm'>${item.movieNm}</sapn>
             <span class='openDt'>${item.openDt}</span>
             <span class='rankInten'>
-            ${item.rankInten > 0 ? 
-            '<span class="spRed">▲</span>' : item.rankInten < 0 ?
-                                              '<span class="spBlue">▼</span>' :'-'}
-             ${item.rankInten!=0?Math.abs(item.rankInten):''}
+            ${item.rankInten > 0 ?
+          '<span class="spRed">▲</span>' : item.rankInten < 0 ?
+            '<span class="spBlue">▼</span>' : '-'}
+             ${item.rankInten != 0 ? Math.abs(item.rankInten) : ''}
             </span>
           </li>`)
       tm = tm.join('')
@@ -72,42 +72,20 @@ const getYesterday = () => {
   //yesterday=`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()-1}`;
   //  const dateFormat=`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()-1}`
   //  dateFormat.setDate(dateFormat.getFullYear())
-
   //yyyy-mm-dd
   return `${year}-${month}-${day}`;
 }
 
-//radio 값 가져오기
-const getGubun=()=>{
-  //radio 요소 가져오기
-  // const r1 = document.querySelector('#r1');
-  // const r2 = document.querySelector('#r2');
-  // const r3 = document.querySelector('#r3');
 
-  // console.log('r1 = ',r1.checked)
-  // console.log('r2 = ',r2.checked)
-  // console.log('r3 = ',r3.checked)
-
-  // if(r1.checked)return r1.value;
-  // else if(r2.checked)return r2.value;
-  // else if(r3.checked)return r3.value;
-
-  //radio 버튼의 클릭된 것만 가져오기
-  const gubun = document.querySelector('input[type=radio]:checked')
-  console.log('gubun = ',gubun.value);
-  return gubun.value;
-}
 
 //DOM 생성후
 document.addEventListener('DOMContentLoaded', () => {
   //요소 가져오기
   const dt = document.querySelector('#dt');
   const ul = document.querySelector('.sec>ul');
-  // const radios = document.querySelectorAll('input[type=radio]')
-  // const radios = document.getElementsByName('mvGubun');
-  const radios = document.querySelectorAll('input[name=mvGubun]');
+  const sel1 = document.querySelector('#sel1');
 
-  
+
   //어제 날짜 구하기
   let yesterday = getYesterday();
   console.log('yesterday : ', yesterday);
@@ -118,32 +96,18 @@ document.addEventListener('DOMContentLoaded', () => {
   //date 기본값
   dt.value = yesterday;
 
-  //gubun 값
-  console.log(getGubun());
-
   //기본 첫 페이지 보이기
-  getData(dt.value.replaceAll('-', ''), ul, getGubun());
+  getData(dt.value.replaceAll('-', ''), ul, sel1.value);
 
   //데이터 가져오기
   dt.addEventListener('change', () => {
-    getData(dt.value.replaceAll('-', ''), ul, getGubun());
+    getData(dt.value.replaceAll('-', ''), ul, sel1.value);
 
-  
+
   });
-  // r1.addEventListener('change', () => {
-  //   getData(dt.value.replaceAll('-', ''), ul, getGubun);
-  // });
-  // r2.addEventListener('change', () => {
-  //   getData(dt.value.replaceAll('-', ''), ul, getGubun);
-  // });
-  // r3.addEventListener('change', () => {
-  //   getData(dt.value.replaceAll('-', ''), ul, getGubun);
-  // });
-  for(let radio of radios){
-    radio.addEventListener('click',()=>{
-     if(radio.checked)getData(dt.value.replaceAll('-', ''), ul, radio.value);
-    });
-  }
+  sel1.addEventListener('change', () => {
+    getData(dt.value.replaceAll('-', ''), ul, sel1.value);
+  });
 
 });
 
